6eabef1b26cd3874d7fc700474d43e94e9ae585e
vladstt <mihaivladro@yahoo.com>
2015-04-25 15:00:52 +0200

    finished with wizard and added modal windows

--- app/assets/javascripts/pushrr.js
+++ app/assets/javascripts/pushrr.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
         setupLabel();
     });
     setupLabel();
+
+    $('.test-click').click(function() {
+        $(this).toggleClass('selected-card');
+        $(".selected-install").toggle();
+    });
+
+    $('.test-error').click(function() {
+        $('.selected-install').hide();
+        $('.install-error').show();
+    })
+
+    /* Centering the modals */
+    $('.modal').on('show.bs.modal', centerModals);
+    $(window).on('resize', centerModals);
+
 });
 
 function setupLabel() {
@@ -23,3 +38,14 @@ function setupLabel() {
         });
     };
 };
+
+/* center modal */
+function centerModals(){
+    $('.modal').each(function(i){
+        var $clone = $(this).clone().css('display', 'block').appendTo('body');
+        var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+        top = top > 0 ? top : 0;
+        $clone.remove();
+        $(this).find('.modal-content').css("margin-top", top);
+    });
+}
